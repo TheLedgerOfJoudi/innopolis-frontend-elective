f320bf4fe87ced5d1390f5eae326dc1000da9c4e
TheLedgerOfJoudi <hadi.mohammad.saleh1@gmail.com>
2023-03-20 11:57:46 +0300

    Updates

--- _app/immutable/chunks/5.095295d2.js
+++ _app/immutable/chunks/5.095295d2.js
@@ -1,1 +1,1 @@
-import{_ as r}from"./_page.40474c2a.js";import{default as t}from"../entry/info-page.svelte.9b6c31b0.js";export{t as component,r as universal};
+import{_ as r}from"./_page.cd42f2ff.js";import{default as t}from"../entry/info-page.svelte.9b6c31b0.js";export{t as component,r as universal};
